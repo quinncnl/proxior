2651fad0065e99e52193be94e79f6a7c1e2ecf28
Tsai Tsing <clear.tsai@gmail.com>
2013-07-24 00:16:10 +0800

    branch mistake

--- match.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MATCH_H_
-#define _MATCH_H_
-
-struct proxy_t *match_list(char *url);
-
-#endif /* _MATCH_H_ */
-
--- rpc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RPC_H_
-#define _RPC_H_
-
-#include "common.h"
-
-void rpc(struct bufferevent *, void *);
-
-#endif /* _RPC_H_ */
-
